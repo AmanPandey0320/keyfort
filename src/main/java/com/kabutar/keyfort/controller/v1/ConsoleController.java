cd82d169320f2473be7890c2d24b8028a6b6ac55
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-05-09 23:40:59 +0530

    init home url

---
+++ src/main/java/com/kabutar/keyfort/controller/v1/ConsoleController.java
@@ -0,0 +1,25 @@
+package com.kabutar.keyfort.controller.v1;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kabutar.keyfort.util.ResponseHandler;
+
+@RestController
+@RequestMapping("/api/v1/console/{dimension}")
+public class ConsoleController {
+	
+	@GetMapping("/home")
+	public ResponseEntity<?> getConsoleData(@PathVariable("dimension") String dimension){
+		return new ResponseHandler()
+		.data(List.of("data",dimension))
+		.status(HttpStatus.OK)
+		.build();
+	}
+}
