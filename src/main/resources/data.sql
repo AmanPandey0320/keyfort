8d3120fb74e04e66e61455f9dcebf02a471f5a59
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-05-10 00:03:55 +0530

    redirect uri changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@
 INSERT IGNORE INTO dimension (id, name,is_active,display_name) VALUES ('e7b1d516-a4d3-42a4-addb-07288b3e2535', 'super', true,'Super');
 
 -- populate master client in db;
-INSERT IGNORE INTO client (client_id,name,client_secret,grant_type,redirect_uri,dimension_id) VALUES ('203b3aba-2c62-4404-9566-0fb77f91d8a0','master','0a5f4360-ae03-4f8a-8020-189a934a4e05-9873e4ca-403b-4ee6-99b3-2c6e7b7e380d','authorization_code','http://localhost:3000/*','e7b1d516-a4d3-42a4-addb-07288b3e2535');
+INSERT IGNORE INTO client (client_id,name,client_secret,grant_type,redirect_uri,dimension_id) VALUES ('203b3aba-2c62-4404-9566-0fb77f91d8a0','master','0a5f4360-ae03-4f8a-8020-189a934a4e05-9873e4ca-403b-4ee6-99b3-2c6e7b7e380d','authorization_code','http://localhost:8085/*','e7b1d516-a4d3-42a4-addb-07288b3e2535');
 
 -- populate user for master client;
 INSERT IGNORE INTO user (id, email, first_name, is_verified, last_name, username, client_id) VALUES ('9e9d251d-50fe-4e41-a22b-e332b7edbf85', 'admin@email.com', 'Admin', 1, NULL, 'admin', '203b3aba-2c62-4404-9566-0fb77f91d8a0');
