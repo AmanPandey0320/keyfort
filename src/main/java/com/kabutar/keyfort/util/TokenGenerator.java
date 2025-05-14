78389ec1d1e3a460dcf246e6e9350ed455a6f25b
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-05-14 22:31:31 +0530

    added valid uuid

--- src/main/java/com/kabutar/keyfort/util/TokenGenerator.java
+++ src/main/java/com/kabutar/keyfort/util/TokenGenerator.java
@@ -1,22 +1,15 @@
 package com.kabutar.keyfort.util;
 
-import java.security.SecureRandom;
+import java.util.UUID;
 
 public class TokenGenerator {
 
-    private static final String ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final SecureRandom secureRandom = new SecureRandom();
-
     public static String generateToken128() {
-        return generateToken(16) + System.currentTimeMillis(); // 16 chars + timestamp
+        return generateToken() + System.currentTimeMillis();
     }
 
-    private static String generateToken(int length) {
-        StringBuilder token = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = secureRandom.nextInt(ALLOWED_CHARS.length());
-            token.append(ALLOWED_CHARS.charAt(index));
-        }
-        return token.toString();
+    private static String generateToken() {
+        return UUID.randomUUID().toString();
+        
     }
 }
