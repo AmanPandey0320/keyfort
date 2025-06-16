a2a3128d1e94761308955acbfba9d3a2a4762679
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-06-16 23:38:34 +0530

    dto change for pkce flow

--- src/main/java/com/kabutar/keyfort/dto/UserDto.java
+++ src/main/java/com/kabutar/keyfort/dto/UserDto.java
@@ -4,30 +4,40 @@ public class UserDto {
     private String username;
     private String password;
     private String clientId;
+    private String responseType;
+    private String redirectUri;
+    private String scope;
+    private String codeChallange;
     
     public UserDto() {}
 
-    public UserDto(String username, String password, String clientId) {
-        this.username = username;
-        this.password = password;
-        this.clientId = clientId;
-    }
+	public UserDto(String username, String password, String clientId, String responseType, String redirectUri,
+			String scope, String codeChallange) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.clientId = clientId;
+		this.responseType = responseType;
+		this.redirectUri = redirectUri;
+		this.scope = scope;
+		this.codeChallange = codeChallange;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getClientId() {
 		return clientId;
@@ -36,6 +46,37 @@ public class UserDto {
 	public void setClientId(String clientId) {
 		this.clientId = clientId;
 	}
-    
-    
+
+	public String getResponseType() {
+		return responseType;
+	}
+
+	public void setResponseType(String responseType) {
+		this.responseType = responseType;
+	}
+
+	public String getRedirectUri() {
+		return redirectUri;
+	}
+
+	public void setRedirectUri(String redirectUri) {
+		this.redirectUri = redirectUri;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getCodeChallange() {
+		return codeChallange;
+	}
+
+	public void setCodeChallange(String codeChallange) {
+		this.codeChallange = codeChallange;
+	}
+
 }
