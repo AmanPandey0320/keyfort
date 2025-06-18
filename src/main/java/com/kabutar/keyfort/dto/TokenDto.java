55a65389dadb772f0352457d3efeb284c63baf62
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-06-18 23:38:36 +0530

    added code verifier to tken dto

--- src/main/java/com/kabutar/keyfort/dto/TokenDto.java
+++ src/main/java/com/kabutar/keyfort/dto/TokenDto.java
@@ -3,12 +3,14 @@ package com.kabutar.keyfort.dto;
 public class TokenDto {
     private String token;
     private String clientSecret;
+    private String codeVerifier;
     
     public TokenDto() {}
 
-    public TokenDto(String token, String clientSecret) {
+    public TokenDto(String token, String clientSecret, String codeVerifier) {
         this.token = token;
         this.clientSecret = clientSecret;
+        this.codeVerifier = codeVerifier;
     }
 
     public String getToken() {
@@ -26,11 +28,23 @@ public class TokenDto {
 	public void setClientSecret(String clientSecret) {
 		this.clientSecret = clientSecret;
 	}
+	
+	
+
+	public String getCodeVerifier() {
+		return codeVerifier;
+	}
+
+	public void setCodeVerifier(String codeVerifier) {
+		this.codeVerifier = codeVerifier;
+	}
 
 	@Override
 	public String toString() {
-		return "TokenDto [token=" + token + ", clientSecret=" + clientSecret + "]";
+		return "TokenDto [token=" + token + ", clientSecret=" + clientSecret + ", codeVerifier=" + codeVerifier + "]";
 	}
+
+	
 
     
 }
