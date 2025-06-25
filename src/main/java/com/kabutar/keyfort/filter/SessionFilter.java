5c76cab278b92d69d230c27211930997d641a612
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-06-25 21:44:28 +0530

    added web flux

--- src/main/java/com/kabutar/keyfort/filter/SessionFilter.java
+++ src/main/java/com/kabutar/keyfort/filter/SessionFilter.java
@@ -1,13 +1,9 @@
 package com.kabutar.keyfort.filter;
 
-import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -17,7 +13,6 @@ import org.springframework.http.ResponseCookie;
 import com.kabutar.keyfort.constant.AuthConstant;
 import com.kabutar.keyfort.data.entity.Session;
 import com.kabutar.keyfort.data.repository.SessionRepository;
-import com.kabutar.keyfort.http.SessionInjectedHttpRequest;
 
 import reactor.core.publisher.Mono;
 
@@ -46,6 +41,7 @@ public class SessionFilter implements WebFilter  {
 								.findFirst()
 								.orElse(null);
 		if(cookie != null) {
+			exchange.getAttributes().put(AuthConstant.CookieType.SESSION_ID, cookie.getValue());
 			return chain.filter(exchange);
 		}
 		
@@ -66,48 +62,10 @@ public class SessionFilter implements WebFilter  {
         // Add the cookie to the response
         exchange.getResponse().addCookie(responseCookie);
         
-        exchange.getAttributes().put(AuthConstant.CookieType.SESSION_ID, responseCookie);
+        exchange.getAttributes().put(AuthConstant.CookieType.SESSION_ID, responseCookie.getValue());
 		
 		return chain.filter(exchange);
 		
 	}
-
-
-
-//	@Override
-//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//			throws ServletException, IOException {
-//		Cookie[] cookies = request.getCookies();
-//		boolean isSessionPresent = false;
-//		
-//		if(cookies != null) {
-//			for(Cookie c: cookies) {
-//				if(c.getName().equals(AuthConstant.CookieType.SESSION_ID)) {
-//					isSessionPresent = true;
-//				}
-//			}
-//		}
-//		
-//		if(!isSessionPresent) {
-//			Session session = new Session();
-//			session.setAuthenticated(false);
-//	        session.setCreatedAt(new Date(System.currentTimeMillis()));
-//	        session.setLastUsed(new Date(System.currentTimeMillis()));
-//			
-//	        sessionRepository.save(session);
-//	        
-//	        Cookie sessionCookie = new Cookie(AuthConstant.CookieType.SESSION_ID, session.getId());
-//            sessionCookie.setHttpOnly(true);
-//            sessionCookie.setPath("/");
-//            sessionCookie.setMaxAge(AuthConstant.ExpiryTime.SESSION);
-//            
-//            response.addCookie(sessionCookie);
-//            
-//            request = new SessionInjectedHttpRequest(request,sessionCookie);
-//		}
-//		
-//		filterChain.doFilter(request, response);
-//		
-//	}
 
 }
